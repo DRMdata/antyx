fcd7314a5bd85c87a6cb4d052fbd993c133a3819
DRMdata <drm.datos@gmail.com>
2025-12-30 06:52:52 +0100

    Actualización

--- antix/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.1"
-__name__ = "antyx"
-__author__ = "Daniel Rodrigálvarez Morente"
-__email__ = "drm.datos@gmail.com"
--- setup.py
+++ setup.py
@@ -4,15 +4,11 @@ setup(
     name="antyx",
     version="0.1.1",
     packages=find_packages(),
-    install_requires=[
-        "pandas>=2.0.0",
-        "numpy>=1.24.0",
-    ],
     author="Daniel Rodrigálvarez Morente",
     author_email="drm.datos@email.com",
     description="Paquete para análisis exploratorio de datos",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/drmdata/antyx",
-    python_requires=">=3.8",
+    python_requires=">=3.8", install_requires=['seaborn', 'seaborn']
 )
